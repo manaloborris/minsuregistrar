8db13d5fc80837b907106fe0dd34e7c383e112d5
Borris <manaloborris153@gmail.com>
2026-04-23 01:14:40 +0800

    Remove custom startup and debug files to restore default Azure PHP runtime

--- test-php.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "PHP is working!<br>";
-echo "REQUEST_URI: " . ($_SERVER['REQUEST_URI'] ?? 'NOT SET') . "<br>";
-echo "SCRIPT_FILENAME: " . ($_SERVER['SCRIPT_FILENAME'] ?? 'NOT SET') . "<br>";
-echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? 'NOT SET') . "<br>";
-echo "PHP_SELF: " . ($_SERVER['PHP_SELF'] ?? 'NOT SET') . "<br>";
-echo "Current working directory: " . getcwd() . "<br>";
-echo "index.php exists: " . (file_exists('index.php') ? 'YES' : 'NO') . "<br>";
